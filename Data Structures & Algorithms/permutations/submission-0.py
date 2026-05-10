1e97ea2babd3c202b086c7c0dcda42038ac1930a
Ishaan Saraswat <127280822+ishaan123456789o@users.noreply.github.com>
2026-05-10 12:48:40 -0700

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def backtrack(path, remaining):
+            nonlocal res
+            if len(path) == len(nums):
+                res.append(path)
+                return
+            for i in range(len(remaining)):
+                backtrack(path + [remaining[i]], remaining[:i] + remaining[i+1:])
+        backtrack([], nums)
+        return res
+        
